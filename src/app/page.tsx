0dd82e7ff7d769a97593f4452b7c0bcb5ed2b50e
Shah Faisal <faisal.islam.ceme@gmail.com>
2024-11-23 12:37:59 +0500

    Escaped apostrophe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HomeComponent from "@/components/home";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center mt-4 ml-4">
-      <h1 className="mb-4 text-2xl font-bold">Welcome to Faisal's World!</h1>
+      <h1 className="mb-4 text-2xl font-bold">Welcome to Faisal&apos;s World!</h1>
       <HomeComponent/>
     </div>
   );
